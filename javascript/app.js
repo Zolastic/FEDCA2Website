4345045fc67d72c40504ac3651dbfafe64120435
Zolastic <honhattienlul@gmail.com>
2022-08-07 23:17:20 +0800

    added content to feedback

--- javascript/app.js
+++ javascript/app.js
@@ -1,11 +1,36 @@
 // There are many ways to pick a DOM node; here we get the form itself and the email
 // input box, as well as the span element into which we will place the error message.
-const form  = document.getElementsByTagName('form')[0];
+const form = document.getElementsByTagName('form')[0];
+const letters = /^[A-Za-z]+$/;
+
+const nameOfPerson = document.getElementById('nameOfPerson');
+const nameOfPersonError = document.querySelector('#nameOfPerson + span.error');
 
 const email = document.getElementById('mail');
 const emailError = document.querySelector('#mail + span.error');
 
-email.addEventListener('input', function (event) {
+
+const slider = document.getElementById("websiteRatingSlider");
+const sliderValue = document.getElementById("ratingValue");
+sliderValue.innerHTML = slider.value;
+
+const feedbackTextArea = document.getElementsByTagName("textarea")[0];
+const textAreaCharCounter = document.getElementById("textAreaCharCounter");
+const charMaxLength = feedbackTextArea.getAttribute("maxlength");
+const feedbackTextAreaError = document.querySelector('#feedbackTextArea + span.error');
+
+nameOfPerson.addEventListener('input', function (event) {
+  if (nameOfPerson.value.match(letters)) {
+    nameOfPersonError.textContent = '';
+    nameOfPersonError.className = '';
+  } else {
+    showErrorName();
+    event.preventDefault();
+  }
+});
+
+
+email.addEventListener('input', function (event) {        // 'input' is the event
   // Each time the user types something, we check if the
   // form fields are valid.
 
@@ -13,39 +38,79 @@ email.addEventListener('input', function (event) {
     // In case there is an error message visible, if the field
     // is valid, we remove the error message.
     emailError.textContent = ''; // Reset the content of the message
-    emailError.className = 'error'; // Reset the visual state of the message
+    emailError.className = ''; // Reset the visual state of the message
   } else {
     // If there is still an error, show the correct error
-    showError();
+    showErrorEmail();
   }
 });
 
 form.addEventListener('submit', function (event) {
   // if the email field is valid, we let the form submit
 
-  if(!email.validity.valid) {
+  if (!email.validity.valid) {
     // If it isn't, we display an appropriate error message
-    showError();
+    showErrorEmail();
     // Then we prevent the form from being sent by canceling the event
     event.preventDefault();
   }
 });
 
-function showError() {
-  if(email.validity.valueMissing) {
+
+slider.oninput = function() {
+  sliderValue.innerHTML = this.value;
+}
+
+
+feedbackTextArea.addEventListener('input', function (event) {
+  const target = event.target;  // target is a read-only property of the Event interface. It references to the object onto which the vent was dispatched
+  const currentLength = target.value.length;
+  textAreaCharCounter.innerHTML = `${currentLength} / ${200}`;
+
+  if (currentLength <= 200) {
+    feedbackTextAreaError.textContent = '';
+    feedbackTextAreaError.className = '';
+    feedbackTextAreaError.setAttribute('isvalid', 'true');
+  } else {
+    showErrorFeedbackTextArea();
+    
+
+  }
+});
+
+
+function showErrorName() {
+  if (nameOfPerson.validity.valueMissing) {
+    nameOfPersonError.textContent = 'Please enter a name.'
+  } else if (!nameOfPerson.value.match(letters)) {
+    nameOfPersonError.textContent = 'Alphabets only please!'
+  }
+
+  nameOfPersonError.className = 'error active';
+}
+
+
+function showErrorEmail() {
+  if (email.validity.valueMissing) {
     // If the field is empty,
     // display the following error message.
     emailError.textContent = 'You need to enter an e-mail address.';
-  } else if(email.validity.typeMismatch) {
+  } else if (email.validity.typeMismatch) {
     // If the field doesn't contain an email address,
     // display the following error message.
     emailError.textContent = 'Entered value needs to be an e-mail address.';
-  } else if(email.validity.tooShort) {
+  } else if (email.validity.tooShort) {
     // If the data is too short,
     // display the following error message.
-    emailError.textContent = `Email should be at least ${ email.minLength } characters; you entered ${ email.value.length }.`;
+    emailError.textContent = `Email should be at least ${email.minLength} characters! You entered ${email.value.length}.`;
   }
 
   // Set the styling appropriately
   emailError.className = 'error active';
+}
+
+function showErrorFeedbackTextArea() {
+  feedbackTextAreaError.setAttribute('isvalid', 'false');
+  feedbackTextAreaError.textContent = 'Feedback cannot be more than 200 characters!';
+  feedbackTextAreaError.className = 'error active';
 }
